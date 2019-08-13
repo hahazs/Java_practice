2de977f3c39efd9beabab8c7d8e54f244b821c71
hahazs <42053298+hahazs@users.noreply.github.com>
2019-08-13 23:14:55 +0800

    MainClass的deletebook方法完成

--- BookManage/src/ui/MainClass.java
+++ BookManage/src/ui/MainClass.java
@@ -1,8 +1,9 @@
 package ui;
 
+import com.sun.xml.internal.ws.api.server.LazyMOMProvider;
 import model.Book;
 
-import java.sql.SQLOutput;
+import java.net.ServerSocket;
 import java.util.Scanner;
 
 public class MainClass {
@@ -22,6 +23,7 @@ public class MainClass {
             if(choice == 5)
             {
                 System.out.println("Exit Successfully");
+                System.exit(0);
             }
             else {
                 switch (choice) {
@@ -73,10 +75,48 @@ public class MainClass {
         else {
             System.out.println("超过图书管理上限");
         }
+        printMenu();
     }
     void deleteBook(){
         /*System.out.println("delete");*/
-        System.out.println("请选择删除方式：1-序号/r/n2-书名");
+        System.out.println("请选择删除方式：1-序号   2-书名  3-返回主菜单");
+        Scanner scan = new Scanner(System.in);
+        int choice_del = scan.nextInt();
+        switch (choice_del){
+            case 1:
+                System.out.println("请输入书本序号：");
+                int num_book = scan.nextInt();
+                num_book = orfind(num_book);
+                if(num_book != -1){
+                    for(int i = num_book;i < count;i++){
+                        booklist[i] = booklist[i+1];
+                        count--;
+                        System.out.println("删除成功");
+                        printAllBook();
+                    }
+                }
+                else {
+                    System.out.println("输入非法");
+                }
+                break;
+            case 2:
+                System.out.println("输入书本名称：");
+                String name_book = scan.next();
+                int id = namefind(name_book);
+                for(int i = id;i < count;i++) {
+                    booklist[i] = booklist[i + 1];
+                    count--;
+                    System.out.println("删除成功");
+                    printAllBook();
+                }
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("输入非法");
+                System.out.println("请选择删除方式：1-序号/r/n2-书名");
+        }
+        printMenu();
     }
     void changeBook(){
         System.out.println("change");
@@ -89,6 +129,32 @@ public class MainClass {
         for(int i = 0;i < count;i++){
             System.out.println("第"+(i+1)+"本书：书名"+booklist[i].getBookname()+"作者："+booklist[i].getAuthor()+"价格："+booklist[i].getPrice()+"元");
         }
+    }
+    int orfind(int number){
+        if(number <= count && number > 0){
+            int num_book = number - 1;
+            return num_book;
+        }
+        else {
+            return -1;
+        }
+    }
+    int namefind(String name){
+        int id = -1;
+        for(int i = 0;i < count;i++){
+            if(booklist[i].getBookname().equals(name)){
+                id = i;
+                break;
+            }
+            else if(i < count){
+                continue;
+            }
+            else{
+                System.out.println("未找到想要的书名");
+                break;
+            }
+        }
+        return id;
     }
 
     public static void main(String[] args) {
