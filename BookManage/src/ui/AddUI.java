2884247efff25eb4f097570e0e5c871a96b3f50f
hahazs <42053298+hahazs@users.noreply.github.com>
2019-08-26 22:51:52 +0800

    UI菜单界面完成
    回调仍未添加

--- BookManage/src/ui/AddUI.java
+++ BookManage/src/ui/AddUI.java
@@ -1,4 +1,92 @@
 package ui;
 
-public class AddUI {
+import com.sun.net.httpserver.Authenticator;
+import control.Operator;
+import sun.security.pkcs11.wrapper.CK_SSL3_KEY_MAT_OUT;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddUI extends JFrame {
+    private JPanel contentPane;
+    private JTextField booknameText;
+    private JTextField authorText;
+    private JTextField priceText;
+    /**
+     * launch the application
+     */
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    AddUI frame = new AddUI();
+                    frame.setVisible(true);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    public AddUI(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100,100,450,300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5,5,5,5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JLabel label = new JLabel("请输入您想要的增加的图书：");
+        label.setBounds(105,15,211,21);
+        contentPane.add(label);
+
+        JLabel label_1 = new JLabel("书名：");
+        label_1.setBounds(15,50,81,21);
+        contentPane.add(label_1);
+
+        JLabel label_2 = new JLabel();
+        label_2.setBounds(15,95,81,21);
+        contentPane.add(label_2);
+
+        JLabel label_3 = new JLabel("价格：");
+        label_3.setBounds(15,137,81,21);
+        contentPane.add(label_3);
+
+        booknameText = new JTextField();
+        booknameText.setBounds(115,51,162,27);
+        contentPane.add(booknameText);
+        booknameText.setColumns(10);
+
+        authorText = new JTextField();
+        authorText.setColumns(10);
+        authorText.setBounds(115,92,162,27);
+        contentPane.add(authorText);
+
+        priceText = new JTextField();
+        priceText.setColumns(10);
+        priceText.setBounds(115,134,162,27);
+        contentPane.add(priceText);
+
+        JButton button = new JButton("\u63D0\u4EA4");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    String choice = "增加";
+                    String name = booknameText.getText();
+                    String author = authorText.getText();
+                    float price = Float.parseFloat(priceText.getText());
+                    Operator operator = new Operator();
+                    boolean isSuccess = operator.addBook(name,author,price);
+                    if(isSuccess){
+                        Success success = new Success(choice);
+
+                    }
+                }
+            }
+        });
+    }
 }
--- BookManage/src/ui/MenuUI.java
+++ BookManage/src/ui/MenuUI.java
@@ -45,5 +45,54 @@ public class MenuUI extends JFrame {
         addButton.setBounds(110,25,123,29);
         contentPane.add(addButton);
 
+        JButton deleteButton = new JButton("删除图书");
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DeleteUI deleteUI = new DeleteUI();
+            }
+        });
+        deleteButton.setBounds(110,56,123,29);
+        contentPane.add(deleteButton);
+
+        JButton button_1 = new JButton("修改图书");
+        button_1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ChangeUI changeUI = new ChangeUI();
+            }
+        });
+        button_1.setBounds(110,90,123,29);
+        contentPane.add(button_1);
+
+        JButton button_2 = new JButton("查询图书");
+        button_2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FindmenuUI findmenuUI = new FindmenuUI();
+            }
+        });
+        button_2.setBounds(110,124,123,29);
+        contentPane.add(button_2);
+
+        JButton button_3 = new JButton("清除图书");
+        button_3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClearUI clearUI = new ClearUI();
+            }
+        });
+        button_3.setBounds(110,157,123,29);
+        contentPane.add(button_3);
+
+        JButton button_4 = new JButton("退出系统");
+        button_4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                MenuUI.this.dispose();
+            }
+        });
+        button_4.setBounds(110,189,123,29);
+        contentPane.add(button_4);
     }
 }
